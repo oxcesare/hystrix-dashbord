10f8e0a5b74b3c21fe9afc61aff23fdec9fc3848
cesaralducinruiz <cesaralducinruiz@192.168.1.64>
2020-04-21 11:10:26 -0500

    Initial

---
+++ src/main/java/io/schultz/dustin/hystrixdashbord/HystrixDashbordApplication.java
@@ -0,0 +1,15 @@
+package io.schultz.dustin.hystrixdashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashbordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashbordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/schultz/dustin/hystrixdashbord/HystrixDashbordApplicationTests.java
@@ -0,0 +1,13 @@
+package io.schultz.dustin.hystrixdashbord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashbordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
